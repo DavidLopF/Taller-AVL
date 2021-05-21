dea7007c60616a420b003db4b870294eb73197be
gitForDavid <dlopezf@unbosque.edu.co>
2021-05-21 15:35:51 -0500

    metodos de insertar, buscar, y recorridos hechos mas no implementados

---
+++ src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,14 @@
+package co.edu.unbosque.controller;
+
+import co.edu.unbosque.model.ArbolAVL;
+import co.edu.unbosque.view.View;
+
+public class Controller {
+
+    private ArbolAVL arbolAVL;
+    private View view;
+
+    public Controller() {
+
+    }
+}
